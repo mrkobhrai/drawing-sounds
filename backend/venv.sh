909a15a22587d2ff366120b28ebae9f85c3803b7
jdubkim <jeewoo1998@gmail.com>
2021-10-15 15:21:55 +0100

    Change virtual environment from anaconda to venv

---
+++ backend/venv.sh
@@ -0,0 +1,5 @@
+python3 -m pip install --user virtualenv
+python3 -m venv sound
+source sound/bin/activate
+pip3 install -r requirements.txt
+deactivate
--- venv.sh
+++
@@ -1,3 +0,0 @@
-conda create -n sound python=3.9
-conda activate sound
-pip3 install -r requirements.txt
