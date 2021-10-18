374c2883408cf1cd5bdcbe470b92fd33c397ff11
codrin <cc1619@ic.ac.uk>
2021-10-18 18:22:25 +0100

    [cc1619] [dp1119] [odc19] Add generic parameter field

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,24 @@
-import React, {createRef} from 'react';
+import React, {ChangeEvent, createRef, useState} from 'react';
 import './App.css';
 import InputGraph from "./InputGraph";
 
 function App() {
+  const [parameter, setParameter] = useState(1)
   let ref = createRef<InputGraph>();
-  const inputGraph = <InputGraph ref={ref}/>
+  const [inputGraph, setInputGraph] = useState(<InputGraph ref={ref}/>)
+
+  const updateParameter: (event: ChangeEvent<HTMLInputElement>) => void = (event) => {
+    setParameter(parseFloat(event.target.value))
+  }
 
   return <div>
     {inputGraph}
     <button onClick={() => ref.current?.resetPoints()}>RESET</button>
+    <div/>
+    <label> Parameter
+      <input type="number" min={0} max={10} step={0.5} value={parameter} onChange={updateParameter}/>
+      <input type="range" min={0} max={10} step={0.5} value={parameter} onChange={updateParameter}/>
+    </label>
   </div>
 }
-
 export default App;
