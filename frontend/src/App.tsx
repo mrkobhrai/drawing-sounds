7468b331d0689ec0367a0aec887225519182be0a
karan obhrai <mrkobhrai@gmail.com>
2021-10-20 19:22:23 +0100

    [kjo19] Generate pitch varying on sample datapoints

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,31 +7,39 @@ function App() {
   let ref = createRef<InputGraph>();
   const inputGraph = <InputGraph ref={ref}/>;
 
-  const osc = new Tone.Oscillator().toDestination();
-  // repeated event every 8th note
-  Tone.Transport.schedule((time) => {
-    osc.start(time).stop(time + 0.1);
-  }, "8n");
+  const pitchShifter = new Tone.PitchShift().toDestination();
+  const osc = new Tone.Oscillator().connect(pitchShifter);
 
-  function play() {
+  const genSound = () => {
+    const points = [{x:0, y:0.1}, {x:0.5, y:12}, {x:1, y:14}, {x:1.5, y:0.1}];
+    for(let i = 0; i < points.length; i++) {
+      const point = points[i];
+      Tone.Transport.schedule((time) => {
+        osc.start(time);
+        if (i < points.length - 1) {
+          const timeOffset = points[i+1].x - point.x;
+          osc.stop(time + timeOffset);
+        } else {
+          osc.stop(0.1);
+        }
+        pitchShifter.pitch = point.y;
+      }, point.x);
+    }
+  }
+
+  genSound();
+
+  const play = () => {
     Tone.start();
-    Tone.Transport.start();
-  }
-
-  function pause() {
-    Tone.Transport.stop();
-  }
-
-  function reset() {
     Tone.Transport.seconds = 0;
+    Tone.Transport.start(Tone.now());
   }
 
   return <div>
     {inputGraph}
     <button onClick={() => ref.current?.resetPoints()}>RESET</button>
     <button onClick={play}>PLAY</button>
-    <button onClick={pause}>PAUSE</button>
-    <button onClick={reset}>RESET</button>
+    <button onClick={genSound}>GENERATE</button>
   </div>
 }
 
