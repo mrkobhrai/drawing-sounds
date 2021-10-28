d85b3ac8cc8a89834c2b0734f652ab300a36e3b4
karan obhrai <mrkobhrai@gmail.com>
2021-10-28 14:40:14 +0100

    Add bounded axis and click handler for sound graph

--- frontend/src/SoundGraph.tsx
+++ frontend/src/SoundGraph.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
 
 interface Props {
     width: number,
@@ -8,14 +8,33 @@ interface Props {
 }
 
 class SoundGraph extends React.Component<Props> {
+    maxX = 5
+    maxY = 10
+    width = this.props.width
+    height = this.props.height
+    
+    handleClick = (e:any) => {
+        if (e) {
+            const xCoord = e.chartX;
+            const yCoord = e.chartY;
+            const x = this.calcXFromXCoord(xCoord);
+            const y = this.calcYFromYCoord(yCoord);
+            console.log(x, y);
+        }
+    };
+
+    calcXFromXCoord = (xCoord: number) => xCoord / this.width * this.maxX
+
+    calcYFromYCoord = (yCoord: number) => this.maxY - (yCoord / this.height * this.maxY)
+
     render () {
             const data = this.props.getDataFunc();
             return (
                 <div style={{marginLeft:"150px"}}>
-                    <LineChart width={this.props.width-150} height={this.props.height} data={data}>
+                    <LineChart width={this.width} height={this.height} data={data} onClick={this.handleClick}>
                         <Line type="monotone" dataKey="y" dot={false} />
-                        <XAxis dataKey="x" />
-                        <YAxis />
+                        <XAxis dataKey="x" domain={[0, this.maxX]} />
+                        <YAxis domain={[0, this.maxY]} />
                         <Tooltip />
                     </LineChart>
                 </div>
