bcd3ef381f784ae0aa6bdfa701d1baa2afe23929
codrin <cc1619@ic.ac.uk>
2021-11-16 17:49:48 +0000

    Add option to run commands on zsh

--- start.sh
+++ start.sh
@@ -1,2 +1,14 @@
-gnome-terminal -x sh -c "cd backend; ./venv.sh; ./run.sh"
-gnome-terminal -x sh -c "cd frontend; yarn install; yarn start"
+if command -v gnome-terminal
+then
+  gnome-terminal -x sh -c "cd backend; ./venv.sh; ./run.sh"
+  gnome-terminal -x sh -c "cd frontend; yarn install; yarn start"
+elif command -v zsh
+then
+  osascript -e 'tell app "Terminal"
+    do script "cd backend; ./venv.sh; ./run.sh"
+  end tell'
+  osascript -e 'tell app "Terminal"
+      do script "cd frontend; yarn install; yarn start"
+    end tell'
+fi
+
