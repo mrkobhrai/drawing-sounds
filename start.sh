8a7702131ac7e1a3e1e086d517ad811ed743f03a
Hyunhoi Koo <hhoikoo00@gmail.com>
2021-11-30 15:43:21 +0000

    [hk619][ag2719] Fix start.sh script to run on single terminal window

--- start.sh
+++ start.sh
@@ -1,14 +1,3 @@
-if command -v gnome-terminal
-then
-  gnome-terminal -x sh -c "cd backend; ./venv.sh; ./run.sh"
-  gnome-terminal -x sh -c "cd frontend; yarn install; yarn start"
-elif command -v zsh
-then
-  osascript -e 'tell app "Terminal"
-    do script "cd backend; ./venv.sh; ./run.sh"
-  end tell'
-  osascript -e 'tell app "Terminal"
-      do script "cd frontend; yarn install; yarn start"
-    end tell'
-fi
+#!/bin/sh
 
+sh -c "cd backend; ./venv.sh; ./run.sh" & sh -c "cd frontend; yarn install; yarn start" ; fg
