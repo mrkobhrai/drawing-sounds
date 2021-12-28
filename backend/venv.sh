6cf4e56ba44072340208b3c6aa109d106d13458d
Hyunhoi Koo <hhoikoo00@gmail.com>
2021-12-29 01:38:04 +0900

    Fix start.sh

--- backend/venv.sh
+++ backend/venv.sh
@@ -1,4 +1,5 @@
 python3 -m pip install --user virtualenv
 python3 -m venv sound
-source sound/bin/activate
+# Command `source` does not work with non-Bash shells
+. sound/bin/activate
 pip3 install -r requirements.txt
